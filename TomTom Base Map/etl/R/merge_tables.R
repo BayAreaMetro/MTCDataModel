93494d50b9f3811b69d98cd1598004cd8af2f935
Tom Buckley <tbuckley@bayareametro.gov>
2017-12-28 11:25:25 -0800

    refactor tomtom etl/merge
    this refactor simplifies and resolves issues in read.dbf() where ID fields were being cast to numeric and being rounded. it solves the latter issue by exporting ogr2ogr bash commands to translate and merge the dbfs, which are executed from R. it simplifies the process by working from abbreviations in the metadata and then scanning for expected files rather than scanning the list of files and deriving the file types from extensions.

--- TomTom Base Map/etl/R/merge_tables.R
+++ TomTom Base Map/etl/R/merge_tables.R
@@ -1,231 +1,180 @@
-library(rgeos)
-library(rgdal)
-library(foreign)
-library(dplyr)
+tt_data_path <- file.path("~/Downloads/zippedArchives/")
+prefix <- "tt_"
 
 ####
-#set up functions
+# Read the Metadata
 ####
 
+library(readr)
+library(readxl)
+library(stringr)
 
-merge_files <- function(tbl1,output_path,product) {
-  paths <- tbl1[['path']]
-  shortname <- paste(output_path,product,tbl1[['shortname']][[1]],".dbf",sep="")
-  print("merging")
-  print(shortname)
-  for (currentFile in paths) {
-    print(currentFile)
-    if (!exists("dataset")){
-      dataset <- read.dbf(currentFile, as.is = FALSE)
-    }
-    # if the merged dataset does exist, a??grou[ppend to it
-    if (exists("dataset")){
-      temp_dataset <-read.dbf(currentFile, as.is = FALSE)
-      dataset<-rbind(dataset, temp_dataset)
-      rm(temp_dataset)
-    }
+shapefile_layers <- read_excel("~../metadata/mapdoc_mn_gdf_3-6-6_vs_mn_shape_4-8_v1-0-1.xlsx",
+                               sheet = "Shapefile Layers",
+                               skip = 2,
+                               col_types=c("text","text","text"),
+                               col_names=c("abbrv","description","feature_type"))
+
+#pad and lower abbreviations to match files
+shapefile_layers$abbrv <- str_pad(shapefile_layers$abbrv, 2, pad="0")
+shapefile_layers$abbrv <- str_to_lower(shapefile_layers$abbrv)
+shapefile_layers$feature_type <- str_to_lower(shapefile_layers$feature_type)
+
+###
+#define a function to get filenames for each shapefile layer
+###
+#' For a given abbreviation, return the filenames that correspond to it
+#'
+#' @param abbrv
+#' @param feature_type
+#' @param tt_data_path
+#' @return vector of filenames
+#' @examples
+#' filenames <- tt_get_filenames(abbrv,feature_type,tt_data_path))
+tt_get_filenames <- function(abbrv, feature_type, thepath) {
+  if (feature_type=="table"){
+    thepattern <- paste0("\\_",abbrv,".dbf")
+    filenames <- list.files(path=thepath, pattern=thepattern, full.names=FALSE)
+  } else
+  {
+    thepattern <- paste0("\\_",abbrv,".shp")
+    filenames <- list.files(path=thepath, pattern=thepattern, full.names=FALSE)
   }
-  print("Writing to disk")
-  print(shortname)
-  write.dbf(file=shortname,dataset)
-  return(data.frame())
 }
 
-merge_files_spatial <- function(tbl1, output_path, product) {
-  paths <- tbl1[['path']]
-  csv_shortname <- paste(output_path,product,
-                         tbl1[['shortname']][[1]],
-                         ".csv",sep="")
-  shp_shortname <- paste("/Users/tommtc/Box/DataViz\ Projects/Data\ Services/2016_12/TomTom2016/merged_shapefiles/",
-                         tbl1[['shortname']][[1]],
-                         ".shp",sep="")
-  print("merging")
-  print(shp_shortname)
-  for (currentFile in paths) {
-    print(currentFile)
-    if (!exists("dataset")){
-      dataset <- st_read(currentFile)
-      print("names")
-      print(names(dataset))
-    }
-    # 
-    if (exists("dataset")){
-      temp_dataset <- st_read(currentFile)
-      st_crs(temp_dataset) <- st_crs(dataset)
-      print(names(dataset))
-      dataset<-rbind(dataset, temp_dataset)
-      rm(temp_dataset)
-    }
-  }
-  print("Writing to disk")
-  print(shp_shortname)
-  st_write(dataset,shp_shortname)
-  st_write(dataset,csv_shortname, 
-           layer_options = "GEOMETRY=AS_WKT", 
-           delete_dsn=TRUE)
-  rowcount <- dims(dataset)[[1]]
-  rm(dataset)
-  return(rowcount)
-}
-
-walk_directory_and_merge_tables <- function(output_path,input_path, product_prefix, test=TRUE) {
-  # Step 1. Turn myTables into a Dataframe with 3 columns c("Folder","Name","Type") Type= Shape or Table
-  myShapes <- list.files(path=source_path, pattern="*.shp", full.names=F, recursive=TRUE)
-  myTables <- list.files(path=source_path, pattern="*.dbf", full.names=F, recursive=TRUE)
-  
-  myShapes2 <- gsub("___________","_", myShapes)
-  myShapes2 <- gsub("____","_", myShapes2)
-  myTables2 <- gsub("___________","_", myTables)
-  myTables2 <- gsub("____","_", myTables2)
-  
-  tbl_df_temp <- strsplit(myTables2, "/|\\\\|\\.|_")
-  shp_df_temp <- strsplit(myShapes2, "/|\\\\|\\.|_")
-  
-  #from https://stackoverflow.com/questions/4227223/r-list-to-data-frame
-  tbl_df1 <- data.frame(t(sapply(tbl_df_temp,c)))
-  shp_df1 <- data.frame(t(sapply(shp_df_temp,c)))
-  
-  names(tbl_df1) <- c('folder1','product','shortname','file_ending')
-  names(shp_df1) <- c('folder1','product','shortname','file_ending')
-  
-  tbl_df1['path'] <- myTables
-  shp_df1['path'] <- myShapes
-  
-  non_spatial_tables <- anti_join(x = tbl_df1, y = shp_df1, by = c("folder1","product","shortname"))
-  
-  if (test==TRUE) {
-    non_spatial_tables <- non_spatial_tables[2:3,]
-  }
-  
-  non_spatial_tables_grouped <- group_by(non_spatial_tables, shortname)
-  
-  setwd(source_path)
-  
-  ####
-  ###do the merge
-  ####
-  
-  non_spatial_tables_grouped %>% do(merge_files(.data,output_path,product=product_prefix))
-}
-
-get_metrics_for_merged_tables <- function(output_path,input_path, product_prefix) {
-  #add metrics:
-  
-  ## Metrics:
-  # How many tables have no records?
-  # 
-  # at each table read: count of records
-  # 
-  # product, name of the features (feature class name), record, type(table/shape)
-  # 
-  # How many records are there in a previous release?
-  # 
-  # How many features are corrupt?
-  
-  merged_tables <- list.files(path=output_path, pattern="*.dbf", full.names=F, recursive=TRUE)
-  
-  merged_tables_tmp <- strsplit(merged_tables, "\\.")
-  tbl_df2 <- data.frame(t(sapply(merged_tables_tmp,c)))
-  shortnames <- tbl_df2[[1]]
-  
-  #get row count for merged tables
-  setwd(output_path)
-  rows <- c()
-  for (filename in merged_tables) {
-    df1 <- read.dbf(filename, as.is = FALSE)
-    rowcount <- dim(df1)[[1]]
-    rows <- append(rows, rowcount)
-  }
-  
-  df_meta <- data.frame(filename=merged_tables,rowcount=rows,shortname=shortnames)
-  
-  setwd("~/Documents/Projects/DataServices/TomTom Base Map/etl")
-  df_meta2 <- read_csv("metadata/2016_input_data_dictionary.csv")
-  df_meta2['shortname'] <- sapply(df_meta2['abbrv'],FUN=tolower)
-  df_meta3 <- inner_join(df_meta,df_meta2, by='shortname')
-  df_meta4 <- df_meta3[,c('description','rowcount','feature_type','filename')]
-  write_csv(df_meta4,paste(output_path,product_prefix,"table_names_and_row_counts.csv",sep=""))
+#' Parse a source filename and return its abbreviation.
+#'
+#'
+#' @param filename a filename of some source data
+#' @return the filename's abbreviated name
+#' @examples
+#' abbreviation <- abbrv('usauc5___________oa05.dbf.gz')
+tt_parse <- function(filename) {
+  filename <- gsub("usa", "", filename)
+  filename <- gsub("gz", "", filename)
+  v1 <- unlist(strsplit(filename, "\\.|_|___________|______________"))
+  return(v1[2])
 }
 
 ####
-## process lpoi
+##apply the filename lookup function, returning filenames
 ####
 
-source_path = "~/Documents/Projects/tomtom_tables/2016_12/nam2016_12/shpd/lpoi"
-output_path = "/Users/tommtc/Box/DataViz\ Projects/Data\ Services/2016_12/TomTom2016/lpoi/"
+shapefile_layers$filenames <- apply(shapefile_layers,1,
+                                    function(x) tt_get_filenames(x['abbrv'],
+                                                                 x['feature_type'],
+                                                                 tt_data_path))
 
-walk_directory_and_merge_tables(output_path, source_path,product_prefix="lpoi_", test=FALSE)
-get_metrics_for_merged_tables(output_path, source_path,product_prefix="lpoi_")
+####
+##segment the files into tables first
+####
+
+table_filenames <- shapefile_layers[with(shapefile_layers,
+                                         feature_type == "table"),]$filenames
+#drop nulls
+vector.is.empty <- function(x) return(length(x) ==0 )
+emptyv <- sapply(table_filenames,vector.is.empty)
+table_filenames <- table_filenames[!emptyv]
+
+###
+#Prep the bash load script
+#to load each first table
+#here we write to sqlite
+#because its easy to append to tables in it
+#later we move these tables to postgres tt_data_pathectly
+###
+first_tables <- unlist(lapply(table_filenames,function(x) x[1]))
+shortnames <- sapply(first_tables, tt_parse, USE.NAMES=FALSE)
+shortnames <- paste0(prefix,shortnames)
+create_tables <- paste("ogr2ogr -f ''SQLite'' db.sqlite -append -nln",
+                       shortnames, first_tables)
 
 ######
-##Process MN
-####
-#copied and pasted from above, should just put this all in a function
+###
+#Prep the bash load script to append all other layers to first
+###
+other_tables <- unlist(lapply(table_filenames,function(x) x[2:length(x)]))
+shortnames <- sapply(other_tables, tt_parse, USE.NAMES=FALSE)
+shortnames <- paste0(prefix,shortnames)
+append_tables <- paste("ogr2ogr -f ''SQLite'' db.sqlite -append -nln",
+                       shortnames, other_tables)
 
-source_path = "~/Documents/Projects/tomtom_tables/2016_12/nam2016_12/shpd/mn"
-output_path = "/Users/tommtc/Box/DataViz\ Projects/Data\ Services/2016_12/TomTom2016/multinet/"
+######
+###
+#Move the merged tables to PostGIS
+###
+first_tables <- unlist(lapply(table_filenames,function(x) x[1]))
+shortnames <- sapply(first_tables, tt_parse, USE.NAMES=FALSE)
+shortnames <- paste0(prefix,shortnames)
 
-walk_directory_and_merge_tables(output_path, source_path,product_prefix="mn_", test=FALSE)
-get_metrics_for_merged_tables(output_path, source_path,product_prefix="mn_")
+local_pg <- "psql -U tom -d tomtom -h 0.0.0.0"
+move_sqlite_to_pg <- paste0("ogr2ogr --config PG_USE_COPY YES -f PGDump ",
+                            "-sql 'select * from ",
+                            shortnames,
+                            "' /vsistdout/ -nln ",
+                            shortnames, " db.sqlite ", "| PGPASSWORD=tomtom ",
+                            local_pg)
 
-####
-##Process mn spatial
-####
+###
+#Do the same for Shapefiles
+#In this case writing back out to Shapefile
+#Alternatively, write to GeoJSON, GeoPackage, tt_data_pathectly to PostGIS, etc
+###
+#shapefiles
+shape_filenames <- shapefile_layers[shapefile_layers$feature_type %in%
+                                      c("point","line","polygon","area"),]$filenames
+emptyv <- sapply(shape_filenames,vector.is.empty)
+shape_filenames <- shape_filenames[!emptyv]
 
-input_path = "~/Documents/Projects/tomtom_tables/2016_12/"
-output_path = "/Users/tommtc/Box/DataViz\ Projects/Data\ Services/2016_12/TomTom2016/mn/"
-product="mn"
+first_tables <- unlist(lapply(shape_filenames,function(x) x[1]))
+shortnames <- sapply(first_tables, tt_parse, USE.NAMES=FALSE)
+shortnames <- paste0(prefix,shortnames)
+create_shapefiles <- paste0("ogr2ogr -f 'ESRI Shapefile' ",
+                            shortnames, ".shp -nln ",
+                            shortnames, " ", first_tables)
 
-setwd(input_path)
-
-df_shp_meta_tmp <- inner_join(shp_df1,df_meta2, by='shortname')
-
-df_shp_meta <- df_shp_meta_tmp[,c('description','shortname','feature_type','path','merge_into')]
-
-df_shp_meta$longname <- gsub(" ","_", df_shp_meta$description)
-
-setwd("~/Documents/Projects/tomtom_tables/2016_12/nam2016_12/shpd/mn")
-
-rows2 <- c()
-df_shp_meta_grouped <- group_by(df_shp_meta, shortname)
-rows2 <- append(rows2,df_shp_meta_grouped %>% do(merge_files_spatial(.data),output_path,product))
-
-####
-##Process poi spatial
-####
-
-input_path = "~/Documents/Projects/tomtom_tables/2016_12/"
-output_path = "/Users/tommtc/Box/DataViz\ Projects/Data\ Services/2016_12/TomTom2016/mn/"
-product="lpoi"
-
-setwd(input_path)
-
-df_shp_meta_tmp <- inner_join(shp_df1,df_meta2, by='shortname')
-
-df_shp_meta <- df_shp_meta_tmp[,c('description','shortname','feature_type','path','merge_into')]
-
-df_shp_meta$longname <- gsub(" ","_", df_shp_meta$description)
-
-setwd("~/Documents/Projects/tomtom_tables/2016_12/nam2016_12/shpd/lpoi")
-
-rows2 <- c()
-df_shp_meta_grouped <- group_by(df_shp_meta, shortname)
-rows2 <- append(rows2,df_shp_meta_grouped %>% do(merge_files_spatial(.data),output_path,product))
+other_tables <- unlist(lapply(shape_filenames,function(x) x[2:length(x)]))
+shortnames <- sapply(other_tables, tt_parse, USE.NAMES=FALSE)
+shortnames <- paste0(prefix,shortnames)
+append_shapefiles <- paste0("ogr2ogr -f 'ESRI Shapefile' ",
+                            shortnames, ".shp -append -nln ",
+                            shortnames, " ", first_tables)
 
 
 
+###
+#Execute the load for each table creation
+###
+#this requires an installation of GDAL
+#optionally, uncomment to write to file and execute at shell
+#write(create_tables, file="move_db_to_sqlite.sh")
+setwd(tt_data_path)
+results <- sapply(create_tables, function(x) try(system(x)))
+is.error <- function(x) inherits(x, "try-error")
+succeeded0 <- !vapply(results, is.error, logical(1))
+get.error.message <- function(x) {attr(x,"condition")$message}
+message0 <- vapply(results[!succeeded0], get.error.message, "")
 
+results <- sapply(append_tables, function(x) try(system(x)))
+is.error <- function(x) inherits(x, "try-error")
+succeeded1 <- !vapply(results, is.error, logical(1))
+get.error.message <- function(x) {attr(x,"condition")$message}
+message1 <- vapply(results[!succeeded1], get.error.message, "")
 
+results <- sapply(move_sqlite_to_pg, function(x) try(system(x)))
+is.error <- function(x) inherits(x, "try-error")
+succeeded2 <- !vapply(results, is.error, logical(1))
+get.error.message <- function(x) {attr(x,"condition")$message}
+message2 <- vapply(results[!succeeded2], get.error.message, "")
 
+results <- sapply(create_shapefiles, function(x) try(system(x)))
+is.error <- function(x) inherits(x, "try-error")
+succeeded3 <- !vapply(results, is.error, logical(1))
+get.error.message <- function(x) {attr(x,"condition")$message}
+message3 <- vapply(results[!succeeded3], get.error.message, "")
 
-
-
-
-
-
-
-
-
-
-
-
+results <- sapply(append_shapefiles, function(x) try(system(x)))
+is.error <- function(x) inherits(x, "try-error")
+succeeded4 <- !vapply(results, is.error, logical(1))
+get.error.message <- function(x) {attr(x,"condition")$message}
+message4 <- vapply(results[!succeeded4], get.error.message, "")
