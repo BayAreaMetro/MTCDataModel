435f1615502e3b73fd2fcf6b473ff4de1c9ed5ac
Kearey Smith <ksmith@mtc.ca.gov>
2018-03-14 15:33:01 -0700

    Update lanes.sql

--- TomTom Base Map/lanes.sql
+++ TomTom Base Map/lanes.sql
@@ -10,6 +10,18 @@ n.oneway,
 n.lanes,
 n.ramp,
 n.partstruc,
+ll.dividertyp,
+ll.seqnr,
+ll.id,
+pm.id,
+li.id,
+ln.id,
+le.id
 FROM tomtom_2016.tt_nw as n LEFT OUTER JOIN
-tomtom_2016.tt_ll as ll ON n.id = ll.id
+tomtom_2016.tt_ll as ll ON n.id = ll.id LEFT OUTER JOIN
+tomtom_2016.tt_ld as ld on n.id = ld.id LEFT OUTER JOIN
+tomtom_2016.tt_ln as ln on n.id = ln.id LEFT OUTER JOIN
+tomtom_2016.tt_li as li on n.id = li.id LEFT OUTER JOIN
+tomtom_2016.tt_pm as pm on n.id = pm.id LEFT OUTER JOIN
+tomtom_2016.tt_le as le on n.id = le.id LEFT OUTER JOIN
 Where n.frc = 0
